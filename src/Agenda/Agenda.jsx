3326358957791d8097650f98685a9e0763a0797e
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-05-12 11:39:03 -0500

    se inicia useEffect y UseState

--- src/Agenda/Agenda.jsx
+++ src/Agenda/Agenda.jsx
@@ -1,61 +1,114 @@
+import { useState, useEffect } from "react";
+
 export function Agenda() {
+  const [nombre, setNombre] = useState("")
+  const [correo, setCorreo] = useState("")
+  const [telefono, setTelefono] = useState("")
+  const [fecha, setFecha] = useState("")
+  const [hora, setHora] = useState("")
 
-    return (
+  return (
+    <>
+      <form>
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <div className="input-group mb-3">
+              <span
+                className="input-group-text bi bi-person-circle"
+                id="basic-addon1"
+              ></span>
+              <input
+                id="nombre"
+                type="text"
+                className="form-control"
+                placeholder="Nombre Cliente"
+                onChange={(evento)=>{
+                    setNombre(evento.target.value)
+                }}
+              />
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+            <div className="input-group mb-3">
+              <span
+                className="input-group-text bi bi-envelope-at-fill"
+                id="basic-addon1"
+              ></span>
+              <input
+                id="correo"
+                type="text"
+                className="form-control"
+                placeholder="Correo cliente"
+                onChange={(evento)=>{
+                    setCorreo(evento.target.value)
+                }}
+              />
+            </div>
+          </div>
+        </div>
 
-        <>
+        <div className="row">
+          <div className="col-12">
+            <div className="input-group mb-3">
+              <span
+                className="input-group-text bi bi-telephone-fill"
+                id="basic-addon1"
+              ></span>
+              <input
+                id="telefono"
+                type="text"
+                className="form-control"
+                placeholder="Telefono Cliente"
+                onChange={(evento)=>{
+                    setTelefono(evento.target.value)
+                }}
+              />
+            </div>
+          </div>
+        </div>
 
-            <form>
-
-                <div className="row">
-                    <div className="col-12 col-md-6">
-                        <div className="input-group mb-3">
-                            <span className="input-group-text bi bi-person-circle" id="basic-addon1"></span>
-                            <input type="text" className="form-control" placeholder="Nombre Cliente" />
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-6">
-                        <div className="input-group mb-3">
-                            <span className="input-group-text bi bi-envelope-at-fill" id="basic-addon1"></span>
-                            <input type="text" className="form-control" placeholder="Correo cliente" />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-12">
-                        <div className="input-group mb-3">
-                            <span className="input-group-text bi bi-telephone-fill" id="basic-addon1"></span>
-                            <input type="text" className="form-control" placeholder="Telefono Cliente" />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-12 col-md-6">
-                        <div className="input-group mb-3">
-                            <span className="input-group-text bi bi-calendar-week-fill" id="basic-addon1"></span>
-                            <input type="date" className="form-control" placeholder="fecha Cita" />
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-6">
-                        <div className="input-group mb-3">
-                            <span className="input-group-text bi bi-alarm-fill" id="basic-addon1"></span>
-                            <select className="form-select"  defaultValue={'DEFAULT'}>
-                                <option value="DEFAULT">Hora:</option>
-                                <option value="1">6:30 am</option>
-                                <option value="2">7:00 am</option>
-                                <option value="3">7:30 am</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-
-
-
-            </form>
-
-
-        </>
-    )
-
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <div className="input-group mb-3">
+              <span
+                className="input-group-text bi bi-calendar-week-fill"
+                id="basic-addon1"
+              ></span>
+              <input
+                id="fecha"
+                type="date"
+                className="form-control"
+                placeholder="fecha Cita"
+                onChange={(evento)=>{
+                    setFecha(evento.target.value)
+                }}
+              />
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+            <div className="input-group mb-3">
+              <span
+                className="input-group-text bi bi-alarm-fill"
+                id="basic-addon1"
+              ></span>
+              <select
+                id="hora"
+                onChange={(evento)=>{
+                    setHora(evento.target.value)
+                }}
+                className="form-select"
+                defaultValue={"DEFAULT"}
+              >
+                <option value="DEFAULT">Hora:</option>
+                <option value="1">6:30 am</option>
+                <option value="2">7:00 am</option>
+                <option value="3">7:30 am</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Reservar</button>
+      </form>
+    </>
+  );
 }
---
+++ src/shared/Carga/Carga.jsx
@@ -0,0 +1,19 @@
+import './Carga.css'
+export function Carga(){
+    return(
+        <>
+        <div className="container my-5 p-5 fondo">
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-5">
+              <img
+                src="../../src/assets/gif/barberia3.gif"
+                alt=""
+                className="img-fluid mx-auto d-block"
+              />
+              <p className='text-light'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint sit quas alias est itaque eveniet, amet voluptatibus nostrum ea in laborum blanditiis, voluptatum natus accusantium nisi aut, atque maiores consectetur sed. Voluptas dolores, sit amet nemo, exercitationem excepturi expedita facere rem non veniam voluptatum, incidunt ex officiis nisi vero! Explicabo.</p>
+            </div>
+          </div>
+        </div>
+        </>
+    )
+}
